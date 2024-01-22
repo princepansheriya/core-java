bfab65a012228449cd67ef5f95fcdfd2c26623a5
princepansheriya <141042319+princepansheriya@users.noreply.github.com>
2024-01-22 17:40:54 +0530

    Changed version from 11.0 to 1.0 in the documentation line.

--- Practical1.java
+++ Practical1.java
@@ -4,7 +4,7 @@ package com.corejava.java;
  * This is a simple Java class (Practical1) that prints "Hello world."
  * 
  * @author prince
- * @version 11.0
+ * @version 1.0
  * @since 1/19/2024
  */
 public class Practical1 {
