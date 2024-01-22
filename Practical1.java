656ba9479a10b5ffc7a3996861726417fe12a41d
princepansheriya <princepansheriya2141@gmail.com>
2024-01-22 17:22:43 +0530

    Practical 1

---
+++ Practical1.java
@@ -0,0 +1,16 @@
+package com.corejava.java;
+
+/**
+ * This is a simple Java class (Practical1) that prints "Hello world."
+ * 
+ * @author prince
+ * @version 11.0
+ * @since 1/19/2024
+ */
+public class Practical1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+	}
+
+}
